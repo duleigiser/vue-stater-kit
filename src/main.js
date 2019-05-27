d2ef7dfb1eea237b3a43f7393498ddc68eb7e1d6
duleigiser <564779666@qq.com>
2019-05-27 16:22:55 +0800

    修改请求

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import infiniteScroll from 'vue-infinite-scroll'
-import {AjaxPlugin, Loading, XButton, ToastPlugin} from 'vux'
+import {Loading, XButton, ToastPlugin} from 'vux'
 // import { AjaxPlugin } from 'vux'
 import './util/rem.js'
 
 Vue.config.productionTip = false
 Vue.use(infiniteScroll)
-Vue.use(AjaxPlugin)// 请求
+// Vue.use(AjaxPlugin)// 请求
 Vue.component('loading', Loading)
 Vue.component('x-button', XButton)
 Vue.use(ToastPlugin)
--- src/util/http.js
+++ src/util/http.js
@@ -1,28 +1,31 @@
 import Vue from 'vue'
+import {AjaxPlugin} from 'vux'
+Vue.use(AjaxPlugin) // 请求
 export default class http {
-  static async request(methods, url, data, loading = false) {
+  static async request (methods, url, data, loading = false) {
     const param = {
       url: url,
-      method: method,
+      methods: methods,
       data: data
     }
-    const res = await Vue.$http(param)
-    if(this.isSuccess(res)) {
+    const res = await Vue.http(param)
+    if (this.isSuccess(res)) {
       return res.data.data
     } else {
       throw this.requestExction(res)
     }
   }
-  static isSuccess({statusCode}) {
-    return statusCode === 200
+  static isSuccess ({status}) {
+    // console.log(status)
+    return status === 200
   }
-  static requestExction(res) {
+  static requestExction (res) {
     return res
   }
   static get (url, data, loading = true) {
-    return this.request('GET', url, data, loading);
+    return this.request('GET', url, data, loading)
   }
   static post (url, data, loading = true) {
-    return this.request('POST', url, data, loading);
+    return this.request('POST', url, data, loading)
   }
 }
